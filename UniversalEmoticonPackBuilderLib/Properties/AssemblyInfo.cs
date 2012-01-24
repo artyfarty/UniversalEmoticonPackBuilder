328bc2ca82f1776bd094fff1b2a8a3877c58d55b
artyfarty <mail@artyfarty.ru>
2012-01-25 00:48:05 +0400

    meta and tag

--- UniversalEmoticonPackBuilderLib/Properties/AssemblyInfo.cs
+++ UniversalEmoticonPackBuilderLib/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("UniversalEmoticonPackBuilderLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UniversalEmoticonPackBuilderLib")]
+[assembly: AssemblyCompany("artyfarty")]
+[assembly: AssemblyProduct("UniversalEmoticonPackBuilder")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
